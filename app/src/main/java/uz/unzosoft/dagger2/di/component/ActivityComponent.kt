df508feb9327fc337340750a68512bfdd9eb2634
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-03-04 16:59:23 +0500

    Into set

--- app/src/main/java/uz/unzosoft/dagger2/di/component/ActivityComponent.kt
+++ app/src/main/java/uz/unzosoft/dagger2/di/component/ActivityComponent.kt
@@ -1,17 +1,4 @@
 package uz.unzosoft.dagger2.di.component
 
-import dagger.Component
-import dagger.android.support.AndroidSupportInjectionModule
-import uz.unzosoft.dagger2.di.module.ActivityModule
-import uz.unzosoft.dagger2.di.module.NetworkModule
-import uz.unzosoft.dagger2.di.module.RepositoryModule
-import javax.inject.Singleton
-
-
-@Component(
-    modules = [AndroidSupportInjectionModule::class,
-        ActivityModule::class]
-)
-@Singleton
 class ActivityComponent {
 }
